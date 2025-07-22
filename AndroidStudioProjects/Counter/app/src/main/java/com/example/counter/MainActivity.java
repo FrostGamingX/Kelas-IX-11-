b076dd775627197337ac536ae40a29773d6f4875
FrostGamingX <frostfrostgamingx@gmail.com>
2025-07-22 19:24:10 +0700

    commit

---
+++ AndroidStudioProjects/Counter/app/src/main/java/com/example/counter/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.counter;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
+    TextView txHasil;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        load();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void load() {
+        txHasil = findViewById(R.id.txHasil);
+        txHasil.setText(String.valueOf(count));
+    }
+
+    public void BtnUP (View view) {
+        count++;
+        txHasil.setText(count+"");
+    }
+
+    public void BtnDOWN (View view) {
+        count--;
+        txHasil.setText(count+"");
+    }
+}
