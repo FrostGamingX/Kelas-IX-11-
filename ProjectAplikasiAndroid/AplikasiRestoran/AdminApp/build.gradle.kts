aceacd4f2371e4610dc44ea96f14a722e8aa644b
FrostGamingX <frostfrostgamingx@gmail.com>
2025-09-16 19:33:37 +0700

    commit

--- ProjectAplikasiAndroid/AplikasiRestoran/AdminApp/build.gradle.kts
+++ ProjectAplikasiAndroid/AplikasiRestoran/AdminApp/build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    "com.google.gms:google-services:4.4.1"
+    "com.google.gms:google-services:4.4.3"
 }
